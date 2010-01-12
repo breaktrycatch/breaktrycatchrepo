36bd72f76d23bd238a273065d014cb570e359019
jon.keon <jon.keon@a30372ee-680a-11de-9593-3d8b33745fea>
2010-01-12 22:15:25 +0000

    JonK - Integrating Tracing View into Paul's Framework

--- projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/core/WeNeedMoreHumansMain.java
+++ projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/core/WeNeedMoreHumansMain.java
@@ -1,16 +1,8 @@
 package com.breaktrycatch.needmorehumans.core;
 
-import hypermedia.video.OpenCV;
-
-import java.awt.Graphics2D;
-
 import processing.core.PApplet;
-import processing.core.PGraphicsJava2D;
-import processing.core.PImage;
 
 import com.breaktrycatch.lib.display.Stage;
-import com.breaktrycatch.needmorehumans.control.webcam.ImageSubstractionController;
-import com.breaktrycatch.needmorehumans.view.ViewManager;
 
 public class WeNeedMoreHumansMain extends PApplet
 {
@@ -30,11 +22,8 @@ public class WeNeedMoreHumansMain extends PApplet
 		PApplet.main(new String[] { "--present", "com.breaktrycatch.needmorehumans.core.WeNeedMoreHumansMain" });
 	}
 
-	private PImage _background;
-	private ImageSubstractionController _subtractor;
-	private OpenCV _opencv;
+	
 	private Stage _stage;
-	private ViewManager _mainViewManager;
 
 	public WeNeedMoreHumansMain()
 	{
@@ -50,48 +39,11 @@ public class WeNeedMoreHumansMain extends PApplet
 		size(800, 600, P2D);
 		
 		_stage = new NeedMoreHumansCore(this);
-		
-		
-//		_background = loadImage("sunset-beach.jpg");
-//		_background.resize(width, height);
-//		_opencv = new OpenCV(this);
-//		_opencv.capture(width, height);
-//		_opencv.remember();
-//		_subtractor = new ImageSubstractionController(this, createImage(_background.width, _background.height, ARGB));
-	}
 
-	public void keyPressed()
-	{
-		// saves an image is spacebar is hit
-//		if (key == ' ')
-//		{
-//			_subtractor.setBackgroundImage(_opencv.image());
-//		} else if (key == ENTER)
-//		{
-//			println("Building primitives");
-//		}
 	}
 
 	public void draw()
 	{
 		_stage.draw();
-//
-//		int c = (int) map(mouseX, 0, width, -128, 128);
-//		int r = (int) map(mouseY, 0, height, -128, 128);
-//
-//		_opencv.read();
-//		// _opencv.brightness(r);
-//		// _opencv.contrast(c);
-//
-//		_opencv.brightness(10);
-//		_opencv.contrast(13);
-//
-//		println(r + " :  " + c);
-//
-//		PImage frame = _opencv.image();
-//		PImage diffed = _subtractor.createDifferenceMask(frame);
-//
-//		image(_background, 0, 0);
-//		image(diffed, 0, 0);
 	}
 }
---
+++ projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/tracing/PixelDataVO.java
@@ -0,0 +1,13 @@
+package com.breaktrycatch.needmorehumans.tracing;
+
+public class PixelDataVO {
+	
+	public int x;
+	public int y;
+	
+	public double distanceFromPreciseCenter;
+	
+	public PixelDataVO() {
+		
+	}
+}
