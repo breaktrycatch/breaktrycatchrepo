861c5d2c12b03fa23f297fc075c2c6862ea2bd66
mbaker3 <mbaker3@a30372ee-680a-11de-9593-3d8b33745fea>
2010-01-26 03:31:49 +0000

    stubbed out GameView

---
+++ projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/view/GameView.java
@@ -0,0 +1,38 @@
+package com.breaktrycatch.needmorehumans.view;
+
+import processing.core.PApplet;
+
+import com.breaktrycatch.lib.view.AbstractView;
+
+public class GameView extends AbstractView {
+
+	//private PhysicsControl _physControl
+	//private CaptureControl _capControl
+	
+	
+	public GameView() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void initialize(PApplet app) {
+		// TODO Auto-generated method stub
+		super.initialize(app);
+		
+		float halfX = app.width/2.0f;
+		float halfY = app.height/2.0f;
+		
+//		_physControl = new PhysicsControl(app);
+//		_capControl = new CaptureControl(app);
+//		
+//		_physControl.width = 
+//			_capControl.width = 
+//			_capControl.x = halfX;
+//		_physControl.height = 
+//			_capControl.height = halfY;
+		
+//		addChild(_physControl);
+//		addChild(_capControl);
+	}
+
+}
