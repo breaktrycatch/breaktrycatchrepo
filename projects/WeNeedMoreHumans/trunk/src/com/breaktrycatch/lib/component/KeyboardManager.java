6d7ae1a13d4706d325135846da30eef1952d04dd
plemarquand <plemarquand@a30372ee-680a-11de-9593-3d8b33745fea>
2010-01-27 04:28:03 +0000

    - Can now add more than one key listener at a time to the KeyManager

--- projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/lib/component/KeyboardManager.java
+++ projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/lib/component/KeyboardManager.java
@@ -1,34 +1,45 @@
 package com.breaktrycatch.lib.component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
-import com.breaktrycatch.lib.util.callback.ISimpleCallback;
-
 import processing.core.PApplet;
+
+import com.breaktrycatch.lib.util.callback.ISimpleCallback;
 
 public class KeyboardManager implements IManager
 {
 	private PApplet _app;
 
-	private HashMap<Character, ISimpleCallback> _charMap;
-	private HashMap<Character, ISimpleCallback> _onceCharMap;
+	private HashMap<Character, ArrayList<ISimpleCallback>> _charMap;
+	private HashMap<Character, ArrayList<ISimpleCallback>> _onceCharMap;
 	private Character _lastKey;
 
 	public KeyboardManager(PApplet app)
 	{
 		_app = app;
-		_charMap = new HashMap<Character, ISimpleCallback>();
-		_onceCharMap = new HashMap<Character, ISimpleCallback>();
+		_charMap = new HashMap<Character, ArrayList<ISimpleCallback>>();
+		_onceCharMap = new HashMap<Character, ArrayList<ISimpleCallback>>();
 	}
 
 	public void registerKey(char key, ISimpleCallback callback)
 	{
-		_charMap.put(key, callback);
+		if(!_charMap.containsKey(key))
+		{
+			_charMap.put(key, new ArrayList<ISimpleCallback>());
+		}
+		
+		_charMap.get(key).add(callback);
 	}
-
+	
 	public void registerKeyOnce(char key, ISimpleCallback callback)
 	{
-		_onceCharMap.put(key, callback);
+		if(!_onceCharMap.containsKey(key))
+		{
+			_onceCharMap.put(key, new ArrayList<ISimpleCallback>());
+		}
+
+		_onceCharMap.get(key).add(callback);
 	}
 
 	@Override
@@ -42,14 +53,22 @@ public class KeyboardManager implements IManager
 			_lastKey = _app.key;
 			if (_onceCharMap.containsKey(_app.key))
 			{
-				ISimpleCallback callback = _onceCharMap.get(_app.key);
-				callback.execute();
+				ArrayList<ISimpleCallback> callbacks = _onceCharMap.get(_app.key);
+				executeList(callbacks);
 			}
 		}
 
 		if (_app.keyPressed && _charMap.containsKey(_app.key))
 		{
-			ISimpleCallback callback = _charMap.get(_app.key);
+			ArrayList<ISimpleCallback> callbacks = _charMap.get(_app.key);
+			executeList(callbacks);
+		}
+	}
+	
+	private void executeList(ArrayList<ISimpleCallback> callbacks)
+	{
+		for(ISimpleCallback callback : callbacks)
+		{
 			callback.execute();
 		}
 	}
