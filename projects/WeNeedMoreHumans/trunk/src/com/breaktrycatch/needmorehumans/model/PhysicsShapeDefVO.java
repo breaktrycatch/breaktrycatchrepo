1637e7a85c3cb7fcb27e1a71facb5e4815662859
mbaker3 <mbaker3@a30372ee-680a-11de-9593-3d8b33745fea>
2010-01-29 02:33:51 +0000

    More new physicscontrol work...still not great

---
+++ projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/model/PhysicsShapeDefVO.java
@@ -0,0 +1,14 @@
+package com.breaktrycatch.needmorehumans.model;
+
+public class PhysicsShapeDefVO {
+
+	public float density = 1.0f;
+	public float friction = 1.0f;
+	public float restitution = 0.0f;
+	public boolean isSensor = false;
+	
+	public PhysicsShapeDefVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
