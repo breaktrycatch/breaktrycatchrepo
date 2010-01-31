d0e066c4c54cd2ab66dcb8b52b6855d8757667f4
mbaker3 <mbaker3@a30372ee-680a-11de-9593-3d8b33745fea>
2010-01-31 22:01:16 +0000

    Can now drag physics view around (testing navigation interaction)

--- trunk/projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/control/physics/PhysicsControl.java
+++ trunk/projects/WeNeedMoreHumans/trunk/src/com/breaktrycatch/needmorehumans/control/physics/PhysicsControl.java
@@ -1,5 +1,6 @@
 package com.breaktrycatch.needmorehumans.control.physics;
 
+import java.awt.Point;
 import java.util.ArrayList;
 
 import org.jbox2d.dynamics.Body;
@@ -16,6 +17,7 @@ import com.breaktrycatch.needmorehumans.tracing.ImageAnalysis;
 public class PhysicsControl extends DisplayObject {
 
 	private PhysicsWorldWrapper _physWorld;
+	private Point _lastMousePos;
 	
 	public PhysicsControl(PApplet app) {
 		super(app);
@@ -33,10 +35,34 @@ public class PhysicsControl extends DisplayObject {
 		PhysicsShapeDefVO vo = new PhysicsShapeDefVO();
 		vo.density = 0.0f;
 		_physWorld.createRect(0, height - 10, width, height, vo);
-		_physWorld.createRect(0, 0, 50, 50, new PhysicsShapeDefVO());
 		
+		
+		
+		addDebugSmileBoxes();
 		addDebugPolyHuman();
 //		_physWorld.createRect(50, 150, 150, 250, new PhysicsShapeDefVO());
+	}
+	
+	private void addDebugSmileBoxes()
+	{
+		float size = 20;
+		for(int i=0; i < 20; i++)
+		{
+			for(int j=0; j < 10; j++)
+			{
+				float x, y;
+				x = (i * size) + 220;
+				y = (j * size) + 310;
+				Body rect = _physWorld.createRect(x, y, x + size, y + size, new PhysicsShapeDefVO());
+				
+				PImage img = getApp().loadImage("../data/physics/BoxMcSmiles.png");
+				img.loadPixels();
+				ImageFrame sprite = new ImageFrame(getApp(), img);
+				sprite.setRotateAroundCenter(true);
+				add(sprite);
+				rect.setUserData(sprite);
+			}
+		}
 	}
 	
 	private void addDebugPolyHuman()
@@ -46,7 +72,7 @@ public class PhysicsControl extends DisplayObject {
 		
 		ImageFrame sprite = new ImageFrame(getApp(), img);
 		//sprite.setRotateAroundCenter(true);
-		sprite.x = 200;
+		sprite.x = 400;
 		sprite.y = 200;
 		
 		addHuman(sprite);
@@ -71,15 +97,40 @@ public class PhysicsControl extends DisplayObject {
 		// TODO Auto-generated method stub
 		super.draw();
 		
-		for(DisplayObject me : this)
+//		for(DisplayObject me : this)
+//		{
+//			PApplet.println(me);
+//		}
+		
+//		PApplet.println("----------");
+		
+		//Scroll control up and down
+		if(getApp().mouseX > this.x && getApp().mouseX < this.width + this.x && getApp().mousePressed)
 		{
-			PApplet.println(me);
+			if(_lastMousePos != null)
+			{
+				this.y += getApp().mouseY - _lastMousePos.y;
+				this.x += getApp().mouseX - _lastMousePos.x;
+			}
+			_lastMousePos = new Point(getApp().mouseX, getApp().mouseY);
+//			int hitAreaSize = 50;
+//			int scrollSpeed = 2;
+//			if(getApp().mouseY < hitAreaSize)
+//			{
+//				this.y += scrollSpeed;
+//			}
+//			else if(getApp().mouseY > getApp().height - hitAreaSize)
+//			{
+//				this.y -= scrollSpeed;
+//			}
+		}
+		else
+		{
+			_lastMousePos = null;
 		}
 		
-		PApplet.println("----------");
-		
-//		getApp().fill(0xff000000);
-//		getApp().rect(0, 0, width, height);
+		getApp().fill(0xff000000);
+		getApp().rect(0, 0, width, height);
 		
 		_physWorld.step();
 	}
